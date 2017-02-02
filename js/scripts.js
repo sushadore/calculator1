5f59f556a2ed4baf5477208dbe52cb8f7fdab50e
Joshua Fairchild and Susha Dore <joshua@>
2017-02-01 20:23:46 -0800

    add subtraction, multiplication and divisionfunctions

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,19 @@ var add = function(number1, number2) {
 	return number1 + number2;
 }
 
+var subtract = function(number1, number2) {
+	return number1 - number2;
+}
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+}
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+}
+
 var number1 = parseInt(prompt("What is your number?"));
 var number2 = parseInt(prompt("What is your second number?"));
-
-alert(add(number1,number2));
+var result = divide(number1, number2);
+alert(result);
---
+++ readme.md
@@ -0,0 +1,16 @@
+## Calculator
+
+#### Basic calculator developed at Epicodus, 2-1-2017
+
+## Setup
+Clone repository & open with text editor.
+
+## Technologies Used
+* HTML
+* CSS
+* Javascript
+
+### License
+Copyright (c) 2017 Joshua Fairchild, Susha Dore
+
+This software is licensed under the MIT license.
