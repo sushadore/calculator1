29acb1844e738a96da6a5716d79ad4343c2b2f34
Joshua Fairchild and Susha Dore <susha@>
2017-02-01 20:06:19 -0800

    create html for calculator and addition function using JavaScript.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+}
+
+var number1 = parseInt(prompt("What is your number?"));
+var number2 = parseInt(prompt("What is your second number?"));
+
+alert(add(number1,number2));
